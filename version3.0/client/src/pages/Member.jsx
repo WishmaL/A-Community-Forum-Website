28256407cd075278e86ddaffddd70131d5dc76c2
Wishma L Herath <35484440+WishmaL@users.noreply.github.com>
2020-05-04 11:00:42 +0530

    Graph section somewhat done

--- version3.0/client/src/pages/Member.jsx
+++ version3.0/client/src/pages/Member.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import SubNavbar from '../components/Sub-navbar';
-// import Carousel from '../components/Carousel';
+import Graph from '../components/Graphs';
 import Artcle from '../components/Article';
 
 export class Member extends Component {
   render() {
     return (
       <div>
+        
         <SubNavbar />
+        <Graph />
         {/* <Carousel /> */}
         <Artcle />
       </div>
