a73b0230288ccd0b0acd82ab84120fee7b8c8aed
Wishma L Herath <35484440+WishmaL@users.noreply.github.com>
2020-06-05 15:49:43 +0530

    Update ShowTimeline.jsx
    Add scrolloing  effect to the timeline

--- version3.0/client/src/components/ShowTimeline.jsx
+++ version3.0/client/src/components/ShowTimeline.jsx
@@ -24,29 +24,44 @@ export const ShowTimeline = () => {
 
   return (
     <div>
+      <style type="text/css">
+        {`
+                  .my_class {
+                    height:800px;
+                    overflow:scroll
+                  }
+                  `}
+      </style>
       <Container>
         <div className="alert alert-primary" role="alert">
           <h1>The Timeline</h1>
         </div>
         <AddTimeline set_timeEvents={updateEvents} />
-        {timeEvents.map((timeEvent) => {
-          return (
-            <Media key={timeEvent.id}>
-              <img
-                width={64}
-                height={64}
-                className="align-self-start mr-3"
-                src="http://lorempics.com/64x64/222831/f1d1d1"
-                alt="Generic placeholder"
-              />
-              <Media.Body>
-                <h5>{timeEvent.topic}</h5>
-                <p>{timeEvent.description}</p>
-                <p>{timeEvent.date}</p>
-              </Media.Body>
-            </Media>
-          );
-        })}
+        <div className="my_class">
+          {timeEvents.map((timeEvent) => {
+            return (
+              // <div>
+
+              // <div className="my_class">
+              <Media key={timeEvent.id}>
+                <img
+                  width={64}
+                  height={64}
+                  className="align-self-start mr-3"
+                  src="http://lorempics.com/64x64/222831/f1d1d1"
+                  alt="Generic placeholder"
+                />
+                <Media.Body>
+                  <h5>{timeEvent.topic}</h5>
+                  <p>{timeEvent.description}</p>
+                  <p>{timeEvent.date}</p>
+                </Media.Body>
+              </Media>
+              // {/* </div> */}
+              // </div>
+            );
+          })}
+        </div>
       </Container>
     </div>
   );
